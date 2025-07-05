de7c08b02dab7add307687342b2ae316c85e40aa
BaekMinHyeok <minhyeokbaek100@gmail.com>
2025-07-05 15:27:40 +0900

    feat: add math utils for test

---
+++ utils/math.ts
@@ -0,0 +1,15 @@
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function isPositive(n: any): boolean {
+  if (n > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
